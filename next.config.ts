1ec063e61329a263731058076795bd3459323835
Natal Madekkal <natalmadekkal.2005@gmail.com>
2026-04-02 01:17:24 +0530

    fix: remove invalid next config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ['10.84.190.125'],
-  },
+
 };
 
 export default nextConfig;
